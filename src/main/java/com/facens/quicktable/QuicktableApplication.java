776aa1a4f73f6be5e65c04fc5e81dae3b922aacb
Marcos Duarte <pe.marcos30@gmail.com>
2024-03-26 20:27:24 -0300

    Adicionando primeiros arquivos

---
+++ src/main/java/com/facens/quicktable/QuicktableApplication.java
@@ -0,0 +1,13 @@
+package com.facens.quicktable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicktableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicktableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facens/quicktable/QuicktableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facens.quicktable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicktableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
